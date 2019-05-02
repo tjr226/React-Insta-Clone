a1097be3d9bce27a5ae44ddbc033fcb208d41c1c
tjr226 <tjr226@gmail.com>
2019-05-02 08:49:55 -0400

    search bar first draft
    implemented first non-functioning search bar draft. 5x total images for logos and icons, UNICODE magnifying glass character in search bar placeholder, basic CSS formatting. looks good for a first draft

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import "./SearchBar.css"
-import instaLogo from '../images/instagram-logo.png'
 
+// importing images
+import instaLogo from '../images/instagram-logo.png'
+import instaCursive from '../images/instagram-cursive.png'
+import heartIcon from '../images/heart.png'
+import compassIcon from '../images/navigation.png'
+import userIcon from '../images/user-outline.png'
 
 function SearchBar(props) {
     return (
-        <div className="search-bar">
-            <img src={instaLogo} alt="instagram logo img" className="logo-img"/>
-            <p>insta logo placeholder</p>
-            <input></input>
+        <div className="search-container">
+            <img src={instaLogo} alt="instagram logo img" className="insta-logo-img"/>
+            <p className="pipe">|</p>
+            <img src={instaCursive} alt="instagram logo cursive"  className="insta-logo-cursive"/>
+            <input className="search-input" placeholder="&#128269; Search"></input>
+            <div className="icon-section">
+                <img src={compassIcon} alt="compass icon" className="search-bar-icon" />
+                <img src={heartIcon} alt="heart icon" className="search-bar-icon" />
+                <img src={userIcon} alt="user icon" className="search-bar-icon" />
+            </div>
         </div>
     )   
 }
