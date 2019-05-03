89b1251cf0d27d736798e509a6fa337b72313e70
tjr226 <tjr226@gmail.com>
2019-05-03 14:26:09 -0400

    first draft of Login.js created

---
+++ instagram-app/src/components/Login/Login.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import instaLogo from '../images/instagram-cursive.png'
+
+class Login extends React.Component {
+    login() {
+        localStorage.setItem('username', "username");
+    }
+
+    
+
+    render() {
+        return (
+            <div className="login-card">
+                <img
+                    src={instaLogo}
+                    alt="instagram cursive logo"
+                    className="login-logo"
+                />
+                <form
+                    className="login-form"
+                    onSubmit={this.login} >
+                    <input
+                        className="login-input-field"
+                        type="text"
+                        placeholder="username"
+                    />
+                    <input
+                        className="login-input-field"
+                        type="text"
+                        placeholder="password"
+                    />
+                    <button>Log In</button>
+
+                </form>
+
+            </div >
+        )
+    }
+}
+
+
+export default Login
