ee388d849a99d67adbc69285635718328d912772
tjr226 <tjr226@gmail.com>
2019-05-04 12:22:46 -0400

    more reformatting
    LikeSection.js reformatted, all PostContainer.css stylings reformatted to styled components

--- instagram-app/src/components/CommentSection/LikeSection.js
+++ instagram-app/src/components/CommentSection/LikeSection.js
@@ -1,39 +1,54 @@
 import React from 'react'
-
 import heartIcon from '../images/heart.png'
 import messageIcon from '../images/speech-bubble.png'
+import styled from "styled-components";
 
+const PostIcons = styled.div`
+    margin-left: 2%;
+`;
+
+const PostIconImg = styled.img`
+    width: 20px;
+    height: 20px;
+    margin-right: 2%;
+`;
+
+const PostLikes = styled.div`
+    padding-left: 2%;
+    font-weight: bold;
+    font-size: .8rem;   
+`;
 
 class LikeSection extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             likes: this.props.likes,
-        }
+        };
     }
 
     addLike = event => {
         event.preventDefault();
         this.setState((prevState) => {
             return { likes: prevState.likes + 1 }
-        }
-        )
-
+        });
     }
 
     render() {
         return (
             <>
-                <div className="post-icons">
-                    <img
+                <PostIcons>
+                    <PostIconImg
                         onClick={this.addLike}
                         src={heartIcon}
                         alt="heart icon"
-                        className="post-icon"
                     />
-                    <img src={messageIcon} alt="message icon" className="post-icon" />
-                </div>
-                <p className="post-likes">{this.state.likes} likes</p>
+                    <PostIconImg
+                        src={messageIcon}
+                        alt="message icon"
+                    />
+                </PostIcons>
+                <PostLikes>{this.state.likes} likes</PostLikes>
             </>
         )
     }
